9139b271e730003bad095522d7036dc74ca858ee
grellyd <graham.brown137@gmail.com>
2016-09-24 15:10:02 +0000

    Added routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    scope '/api/' do
+        scope '/v1/' do
+            scope '/users' do
+                get '/' => 'users#index'
+                post '/' => 'users#create'
+                put '/' => 'users#update'
+                patch '/' => 'users#update'
+                delete '/' => 'users#destroy'
+            scope '/trips' do
+                get '/' => 'trips#index'
+                post '/' => 'trips#create'
+                put '/' => 'trips#update'
+                patch '/' => 'trips#update'
+                delete '/' => 'trips#destroy'
+            scope '/locations' do
+                get '/' => 'locations#index'
+                post '/' => 'locations#create'
+                put '/' => 'locations#update'
+                patch '/' => 'locations#update'
+                delete '/' => 'locations#destroy'
+            end
+        end
+    end
 end
