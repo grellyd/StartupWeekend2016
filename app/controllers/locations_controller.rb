ff6e796dad8d7f7e9e9729c122d81adbe2d6417d
grellyd <graham.brown137@gmail.com>
2016-09-24 15:57:16 +0000

    Added Controllers for the models

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,13 @@
+class LocationsController < ApplicationController
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,13 @@
+class TripsController < ApplicationController
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
