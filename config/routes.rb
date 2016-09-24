56e8df6597fd46e88879dc3df2481a0dd8ccaa2d
grellyd <graham.brown137@gmail.com>
2016-09-24 15:56:56 +0000

    Created API routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,32 @@ Rails.application.routes.draw do
             scope '/users' do
                 get '/' => 'users#index'
                 post '/' => 'users#create'
-                put '/' => 'users#update'
-                patch '/' => 'users#update'
-                delete '/' => 'users#destroy'
+                scope '/:id' do
+                    get '/' => 'users#show'
+                    put '/' => 'users#update'
+                    patch '/' => 'users#update'
+                    delete '/' => 'users#destroy'
+                end
+            end
             scope '/trips' do
                 get '/' => 'trips#index'
                 post '/' => 'trips#create'
-                put '/' => 'trips#update'
-                patch '/' => 'trips#update'
-                delete '/' => 'trips#destroy'
+                scope '/:id' do
+                    get '/' => 'trips#show'
+                    put '/' => 'trips#update'
+                    patch '/' => 'trips#update'
+                    delete '/' => 'trips#destroy'
+                end
+            end
             scope '/locations' do
                 get '/' => 'locations#index'
                 post '/' => 'locations#create'
-                put '/' => 'locations#update'
-                patch '/' => 'locations#update'
-                delete '/' => 'locations#destroy'
+                scope '/:id' do
+                    get '/' => 'locations#show'
+                    put '/' => 'locations#update'
+                    patch '/' => 'locations#update'
+                    delete '/' => 'locations#destroy'
+                end
             end
         end
     end
